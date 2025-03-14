967d8449209a41b25553664a8e7820f1daa3651f
kenaniscoding <banalkenan@gmail.com>
2025-03-14 21:47:42 +0800

    fix prob stepperMotor

--- stepperMotor.py
+++ stepperMotor.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 # Define pin connections (using BOARD numbering)
-dir_pin = 21    # Physical pin 3
-step_pin = 20   # Physical pin 5
+dir_pin = 21    # Physical pin 21
+step_pin = 20   # Physical pin 20
 steps_per_revolution = 200
 
 # Define absolute positions (steps from home)
