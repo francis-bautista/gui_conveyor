17a99940aa7bac8a343989c7aa600e8e1faae072
kenaniscoding <banalkenan@gmail.com>
2025-03-23 15:41:06 +0800

    change time motor

--- total_all.py
+++ total_all.py
@@ -339,7 +339,7 @@ def update_gui():
     top_background.save(f"{formatted_date_time}_background.png")  # Save the top image for size calculation
     # Capture top part
     moveMotor(0,1,1,0)
-    time.sleep(15)
+    time.sleep(5)
     stopMotor()
     
     print("\nCapturing Top Part")
@@ -354,7 +354,7 @@ def update_gui():
     top_canvas.create_image(0, 0, anchor=tk.NW, image=top_photo)
     top_canvas.image = top_photo
     moveMotor(0,1,0,1)
-    time.sleep(15)
+    time.sleep(5)
     stopMotor()
     # update_video_feed()
     
@@ -386,7 +386,7 @@ def update_gui():
     print(f"Average Final Score: {average_final_grade}")
     find_grade(average_final_grade)
     moveMotor(1,0,1,0)
-    time.sleep(15)
+    time.sleep(5)
     stopMotor()
     print("\nDone")
     
