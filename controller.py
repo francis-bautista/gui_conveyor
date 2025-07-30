11a8fc0ffeefdd8f54253bda9870c08de25cc1bb
kenaniscoding <banalkenan@gmail.com>
2025-07-30 10:44:45 +0800

    added formula_controller.py and removed comments

--- controller.py
+++ controller.py
@@ -6,6 +6,7 @@ from get_size import calculate_size, determine_size
 from motor_controller import MotorController
 from ai_analyzer import AIAnalyzer
 from camera_manager import CameraManager
+from formula_controller import FormulaController
     
 class ConveyorController:
     def __init__(self, app, colors):
@@ -36,6 +37,7 @@ class ConveyorController:
         self.BUTTON_HEIGHT = 40
         self.mc = MotorController()
         self.picam2 = CameraManager()
+        self.formula = FormulaController(self.RIPENESS_SCORES, self.BRUISES_SCORES, self.SIZE_SCORES)
 
         self.init_ui()
     
@@ -351,7 +353,7 @@ class ConveyorController:
                                           font=self.DEFAULT_BOLD)
         self.button_enter.grid(row=row_index, column=col_index, padx=PADDING_X_Y,
                                pady=PADDING_X_Y, sticky="nswe", columnspan=3)
-
+        # TODO: put the formula controller
         row_index+=1
         self.button_help = ctk.CTkButton(frame_choices, text="Help", 
                                          command=self.get_help_page_info,
@@ -447,6 +449,7 @@ class ConveyorController:
             self.bruises_combo.configure(state="disabled")
             self.size_combo.configure(state="disabled")
             self.button_enter.configure(text="Cancel")
+            self.formula.set_input_priority(self.get_input_priorities())
             self.priority_enabled = False
         
     def reset_program(self):
@@ -486,7 +489,7 @@ class ConveyorController:
         predicted = {'r':top_class_ripeness, 'b': top_class_bruises, 's': top_size_class}
         top_final_grade = self.ai.get_overall_grade(predicted, priorities)
         self.top_final_score=top_final_grade
-        top_letter_grade = self.get_grade_letter(top_final_grade)
+        top_letter_grade = self.formula.get_grade_letter(top_final_grade)
         self.set_textbox_results(top_image, top_class_ripeness,
                                  top_class_bruises, top_size_class,
                                  top_final_grade, top_letter_grade, True)
@@ -515,13 +518,13 @@ class ConveyorController:
         predicted = {'r':bottom_class_ripeness, 'b': bottom_class_bruises, 's': bottom_size_class}
         bottom_final_grade = self.ai.get_overall_grade(predicted, priorities)
         self.bottom_final_score=bottom_final_grade
-        bottom_letter_grade = self.get_grade_letter(bottom_final_grade)
+        bottom_letter_grade = self.formula.get_grade_letter(bottom_final_grade)
         self.set_textbox_results(bottom_image, bottom_class_ripeness, bottom_class_bruises,
                                  bottom_size_class, bottom_final_grade, bottom_letter_grade,
                                  False)
         
         average_score = (self.top_final_score + self.bottom_final_score) / 2
-        average_letter = self.get_grade_letter(average_score)
+        average_letter = self.formula.get_grade_letter(average_score)
         
         self.results_data.configure(
             text=(f"Average Score: {average_score:.2f}\n" + 
@@ -540,51 +543,12 @@ class ConveyorController:
         for button, state in button_configs.items():
             button.configure(state=state)
      
-    def get_grade_letter(self, input_grade):
-        priorities = self.get_input_priorities()
-        boundaries = self.get_grade_formula(priorities)
-        self.print_grade_formula(boundaries)
-        
-        return self.get_grade_with_formula(input_grade, boundaries)
-
     def get_input_priorities(self):
-        return {
+        arr = {
             'ripeness': float(self.ripeness_combo.get()),
             'bruises': float(self.bruises_combo.get()),
             'size': float(self.size_combo.get())}
-
-    def get_grade_formula(self, priorities):
-        max_score = (priorities['ripeness'] * self.RIPENESS_SCORES['green'] + 
-                    priorities['bruises'] * self.BRUISES_SCORES['unbruised'] + 
-                    priorities['size'] * self.SIZE_SCORES['large'])
-        
-        min_score = (priorities['ripeness'] * self.RIPENESS_SCORES['yellow'] + 
-                    priorities['bruises'] * self.BRUISES_SCORES['bruised'] + 
-                    priorities['size'] * self.SIZE_SCORES['small'])
-        
-        segment_size = (max_score - min_score) / 3
-        
-        return {
-            'A': {'min': max_score - segment_size, 'max': max_score},
-            'B': {'min': max_score - 2 * segment_size, 'max': max_score - segment_size},
-            'C': {'min': min_score, 'max': max_score - 2 * segment_size}
-        }
-
-    def print_grade_formula(self, boundaries):
-        print("Calculated Grade Range")
-        for grade in ['A', 'B', 'C']:
-            min_val = boundaries[grade]['min']
-            max_val = boundaries[grade]['max']
-            range_size = max_val - min_val
-            print(f"Grade {grade}: {max_val:.2f} - {min_val:.2f}, Range: {range_size:.2f}")
-
-    def get_grade_with_formula(self, input_grade, boundaries):
-        if boundaries['A']['min'] <= input_grade <= boundaries['A']['max']:
-            return "A"
-        elif boundaries['B']['min'] <= input_grade < boundaries['B']['max']:
-            return "B"
-        else:
-            return "C"    
+        return arr
     
     def set_textbox_results(self, image, ripeness, bruises, size, score, letter, is_top):
         def update():
@@ -698,15 +662,6 @@ class ConveyorController:
             self.set_error_pop_up(top_parent, "ERROR: Not a number",
                                             "Please enter a number.")
             textbox.configure(state="normal")
-
-    # def get_video_feed(self):
-    #     vid_params = {'f_length':300, 'f_width':200, 'buffer':10, 'x':0, 'y':0}
-    #     frame = self.picam2.get_image()
-    #     frame = frame.resize((vid_params['f_length'], vid_params['f_width']))
-    #     frame = ImageTk.PhotoImage(frame)
-    #     self.video_canvas.create_image(vid_params['x'], vid_params['y'], anchor=ctk.NW, image=frame)
-    #     self.video_canvas.image = frame
-    #     app.after(vid_params['buffer'], self.get_video_feed)
 
     def run(self):
         self.app.mainloop()
---
+++ formula_controller.py
@@ -0,0 +1,46 @@
+
+class FormulaController:
+    def __init__(self, ripeness, bruises, size):
+        self.RIPENESS_SCORES = ripeness
+        self.BRUISES_SCORES = bruises
+        self.SIZE_SCORES = size
+
+    def get_grade_formula(self, priorities):
+        max_score = (priorities['ripeness'] * self.RIPENESS_SCORES['green'] + 
+                    priorities['bruises'] * self.BRUISES_SCORES['unbruised'] + 
+                    priorities['size'] * self.SIZE_SCORES['large'])
+        
+        min_score = (priorities['ripeness'] * self.RIPENESS_SCORES['yellow'] + 
+                    priorities['bruises'] * self.BRUISES_SCORES['bruised'] + 
+                    priorities['size'] * self.SIZE_SCORES['small'])
+        
+        segment_size = (max_score - min_score) / 3
+        
+        return {
+            'A': {'min': max_score - segment_size, 'max': max_score},
+            'B': {'min': max_score - 2 * segment_size, 'max': max_score - segment_size},
+            'C': {'min': min_score, 'max': max_score - 2 * segment_size}
+        }
+
+    def set_input_priority(self, arr):
+        print(arr)
+        self.input_priorities = arr
+
+    def get_grade_letter(self, input_grade):
+        boundaries = self.get_grade_formula(self.input_priorities)
+        self.print_grade_formula(boundaries)
+        if boundaries['A']['min'] <= input_grade <= boundaries['A']['max']:
+            return "A"
+        elif boundaries['B']['min'] <= input_grade < boundaries['B']['max']:
+            return "B"
+        else:
+            return "C"
+
+    def print_grade_formula(self, boundaries):
+        print("Calculated Grade Range")
+        for grade in ['A', 'B', 'C']:
+            min_val = boundaries[grade]['min']
+            max_val = boundaries[grade]['max']
+            range_size = max_val - min_val
+            print(f"Grade {grade}: {max_val:.2f} - {min_val:.2f}, Range: {range_size:.2f}")
+
