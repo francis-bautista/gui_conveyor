5f5079afdb09a64b5ee1de8a2e066ebbbf30587a
kenaniscoding <banalkenan@gmail.com>
2025-08-21 13:41:47 +0800

    efficientnetb4 from b0

--- ai_analyzer.py
+++ ai_analyzer.py
@@ -33,11 +33,11 @@ class AIAnalyzer:
     
     def load_models(self):
         # TODO: change this one to the specific version of the efficientnet
-        self.model_ripeness = EfficientNet.from_pretrained('efficientnet-b0', num_classes=len(self.RIPENESS_SCORES))
+        self.model_ripeness = EfficientNet.from_pretrained('efficientnet-b4', num_classes=len(self.RIPENESS_SCORES))
         self.model_ripeness.load_state_dict(torch.load("ripeness.pth", map_location=self.device))
         self.model_ripeness.eval()
         self.model_ripeness.to(self.device)
-        self.model_bruises = EfficientNet.from_pretrained('efficientnet-b0', num_classes=len(self.BRUISES_SCORES))
+        self.model_bruises = EfficientNet.from_pretrained('efficientnet-b4', num_classes=len(self.BRUISES_SCORES))
         self.model_bruises.load_state_dict(torch.load("bruises.pth", map_location=self.device))
         self.model_bruises.eval()
         self.model_bruises.to(self.device)
