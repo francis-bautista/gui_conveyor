dd6ef2dd0b8e66b02700b2c8b9f7949faf5c67c7
kenaniscoding <banalkenan@gmail.com>
2025-09-06 15:02:33 +0800

    added folder

--- controller_v2.py
+++ controller_v2.py
@@ -526,8 +526,10 @@ class ConveyorControllerV2:
             f_dt = self.recorded_time
             t_r = self.ai.get_predicted_class(t_img, self.ai.get_is_ripeness())
             t_b = self.ai.get_predicted_class(t_img, self.ai.get_is_bruises())
-            imgs = {'m': f"{f_dt}_top.png", 
-                    'f_dt': f_dt}
+            path_img = os.path.join(self.img_dir, f"{f_dt}_top.png")
+            # imgs = {'m': f"{f_dt}_top.png", 
+            #         'f_dt': f_dt}
+            imgs = {'m': path_img, 'f_dt': f_dt}
             # t_x, t_y = calculate_size(imgs, s1)
             print("\n\nRCNN")
             x, y = self.process_mango_image(imgs['m'])
@@ -562,8 +564,12 @@ class ConveyorControllerV2:
         f_dt = self.recorded_time
         b_r = self.ai.get_predicted_class(b_img, self.ai.get_is_ripeness())
         b_b = self.ai.get_predicted_class(b_img, self.ai.get_is_bruises())
-        imgs = {'m': f"{f_dt}_bottom.png", 
-                'f_dt': f_dt}
+        path_img = os.path.join(self.img_dir, f"{f_dt}_bottom.png")
+        # imgs = {'m': f"{f_dt}_top.png", 
+        #         'f_dt': f_dt}
+        imgs = {'m': path_img, 'f_dt': f_dt}
+        # imgs = {'m': f"{f_dt}_bottom.png", 
+        #         'f_dt': f_dt}
         # b_x, b_y = calculate_size(imgs, s2)
         print("\n\nRCNN")
         x, y = self.process_mango_image(imgs['m'])
