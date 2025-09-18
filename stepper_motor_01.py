e2fbdea0175225f5eca79a0fbec4a82cc2638500
kenaniscoding <banalkenan@gmail.com>
2025-09-18 19:29:56 +0800

    h

--- stepper_motor_01.py
+++ stepper_motor_01.py
@@ -1,46 +1,10 @@
-# from time import sleep
-# import RPi.GPIO as GPIO
-#
-# DIR = 21   # Direction GPIO Pin
-# STEP = 20  # Step GPIO Pin
-# CW = 1     # Clockwise Rotation
-# CCW = 0    # Counterclockwise Rotation
-# SPR = 48   # Steps per Revolution (360 / 7.5)
-#
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(DIR, GPIO.OUT)
-# GPIO.setup(STEP, GPIO.OUT)
-# GPIO.output(DIR, CW)
-#
-# step_count = SPR
-# delay = 0.0208
-#
-# for x in range(step_count):
-#     GPIO.output(STEP, GPIO.HIGH)
-#     sleep(delay)
-#     GPIO.output(STEP, GPIO.LOW)
-#     sleep(delay)
-#
-# sleep(.5)
-# GPIO.output(DIR, CCW)
-# for x in range(step_count):
-#     GPIO.output(STEP, GPIO.LOW)
-#     sleep(delay)
-#     GPIO.output(STEP, GPIO.HIGH)
-#     sleep(delay)
-#
-#
-#
-# GPIO.cleanup()
-
 from time import sleep
 import RPi.GPIO as GPIO
 
-# Pin setup
 DIR = 21   # Direction GPIO Pin
 STEP = 20  # Step GPIO Pin
-CW = 1     # Clockwise Rotation
-CCW = 0    # Counterclockwise Rotation
+CW = 0     # Try flipping logic here
+CCW = 1
 SPR = 48   # Steps per Revolution (360 / 7.5)
 
 GPIO.setmode(GPIO.BCM)
@@ -52,6 +16,7 @@ delay = 0.0208
 
 # Clockwise
 GPIO.output(DIR, CW)
+sleep(0.01)  # allow DIR pin to settle
 for x in range(step_count):
     GPIO.output(STEP, GPIO.HIGH)
     sleep(delay)
@@ -62,8 +27,9 @@ sleep(0.5)
 
 # Counterclockwise
 GPIO.output(DIR, CCW)
+sleep(0.01)
 for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)   # keep same pulse sequence
+    GPIO.output(STEP, GPIO.HIGH)
     sleep(delay)
     GPIO.output(STEP, GPIO.LOW)
     sleep(delay)
