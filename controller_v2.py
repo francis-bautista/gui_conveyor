c13ce5b90db09afd13d1c310535aa3196f423c4b
kenaniscoding <banalkenan@gmail.com>
2025-09-18 11:18:46 +0800

    testing the obj detect with stopper

--- controller_v2.py
+++ controller_v2.py
@@ -43,7 +43,7 @@ class ConveyorControllerV2:
         self.mc = MotorController()
         self.picam2 = CameraManager()
         self.formula = FormulaController(self.RIPENESS_SCORES, self.BRUISES_SCORES, self.SIZE_SCORES)
-        RCNN_PATH = "mango_detection_model.pth"
+        RCNN_PATH = "mango_detection_model_stopper.pth"
         self.rcnn_size = MangoMeasurementSystem(RCNN_PATH)
         self.init_ui()
     
