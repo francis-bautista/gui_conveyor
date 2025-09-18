4b03c217efff2388fe84ce82028b72742823e8ff
kenaniscoding <banalkenan@gmail.com>
2025-09-18 19:28:21 +0800

    cw to ccw

--- stepper_motor_01.py
+++ stepper_motor_01.py
@@ -1,6 +1,42 @@
+# from time import sleep
+# import RPi.GPIO as GPIO
+#
+# DIR = 21   # Direction GPIO Pin
+# STEP = 20  # Step GPIO Pin
+# CW = 1     # Clockwise Rotation
+# CCW = 0    # Counterclockwise Rotation
+# SPR = 48   # Steps per Revolution (360 / 7.5)
+#
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(DIR, GPIO.OUT)
+# GPIO.setup(STEP, GPIO.OUT)
+# GPIO.output(DIR, CW)
+#
+# step_count = SPR
+# delay = 0.0208
+#
+# for x in range(step_count):
+#     GPIO.output(STEP, GPIO.HIGH)
+#     sleep(delay)
+#     GPIO.output(STEP, GPIO.LOW)
+#     sleep(delay)
+#
+# sleep(.5)
+# GPIO.output(DIR, CCW)
+# for x in range(step_count):
+#     GPIO.output(STEP, GPIO.LOW)
+#     sleep(delay)
+#     GPIO.output(STEP, GPIO.HIGH)
+#     sleep(delay)
+#
+#
+#
+# GPIO.cleanup()
+
 from time import sleep
 import RPi.GPIO as GPIO
 
+# Pin setup
 DIR = 21   # Direction GPIO Pin
 STEP = 20  # Step GPIO Pin
 CW = 1     # Clockwise Rotation
@@ -10,25 +46,26 @@ SPR = 48   # Steps per Revolution (360 / 7.5)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
-GPIO.output(DIR, CW)
 
 step_count = SPR
 delay = 0.0208
 
+# Clockwise
+GPIO.output(DIR, CW)
 for x in range(step_count):
     GPIO.output(STEP, GPIO.HIGH)
     sleep(delay)
     GPIO.output(STEP, GPIO.LOW)
     sleep(delay)
 
-sleep(.5)
+sleep(0.5)
+
+# Counterclockwise
 GPIO.output(DIR, CCW)
 for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)   # keep same pulse sequence
+    sleep(delay)
     GPIO.output(STEP, GPIO.LOW)
     sleep(delay)
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-
-
 
 GPIO.cleanup()
