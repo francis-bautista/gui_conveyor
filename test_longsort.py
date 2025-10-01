399d04977c497dfc34c3f052571ceaaa0648311d
kenaniscoding <banalkenan@gmail.com>
2025-10-01 09:17:39 +0800

    test sorting

--- test_longsort.py
+++ test_longsort.py
@@ -8,38 +8,35 @@ states = [
     # individual motor activation
     # only long conveyor towards two options (towards motor)
     [1, 0, 0, 0],
-    # [0, 0, 0, 0],
     # only long conveyor towards A. (against motor)
     [0, 1, 0, 0],
+    # # only short conveyor towards motor
+    # [0, 0, 1, 0],
     # [0, 0, 0, 0],
-    # only short conveyor towards motor
-    [0, 0, 1, 0],
+    # # only short conveyor against motor 
+    # [0, 0, 0, 1],
     # [0, 0, 0, 0],
-    # only short conveyor against motor 
-    [0, 0, 0, 1],
+    #
+    # # multiple motor activation
+    #
+    # [0, 1, 0, 1],
     # [0, 0, 0, 0],
-    
-    # multiple motor activation
-    
-    [0, 1, 0, 1],
+    #
+    # [1, 0, 1, 0],
     # [0, 0, 0, 0],
-
-    [1, 0, 1, 0],
+    #
+    # [0, 1, 1, 0],
     # [0, 0, 0, 0],
-
-    [0, 1, 1, 0],
-    # [0, 0, 0, 0],
-
-    [1, 0, 0, 1]
+    #
+    # [1, 0, 0, 1]
 ]
 
 try:
     for i, state in enumerate(states):
         print(f"State {i+1}: {state}")
         controller.set_motors(state)
-        time.sleep(20)
-        controller.stop_motors()
-        time.sleep(3)
+        time.sleep(60)
+        controller.stop_motors(3)
     
     controller.stop_motors()
     controller.clean_gpio()
